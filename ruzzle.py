edc40e1413960c35406477accb8f74200e0bbe5f
beccosauro <cerioli.simone@gmail.com>
2020-02-16 18:28:13 +0100

    DONE

--- ruzzle.py
+++ ruzzle.py
@@ -1,32 +1,33 @@
 import itertools
-input, size =["walk", "moon", "hate", "rope"], 4
-positions = list(itertools.product(range(size),repeat=2))
-movements = [x for x in itertools.product(range(-1,2),repeat=2) if sum(x)==1 or sum(x)==-1]
-sumTuple = lambda x,y: tuple([sum(x) for x in zip(x,y)])
 
-dictionary = [w.strip() for w in open("dictionary.txt")]
+legalPos = lambda x, seen : 0<=x[0]<matrixSize and 0<=x[1]<matrixSize and x not in seen
+sumTuple = lambda x,y: tuple([sum(x) for x in zip(x,y)])
 getWord = lambda pos: "".join([input[x[0]][x[1]] for x in pos])
 
 def getNeighbour(pos,seen):
-    return [sumTuple(pos,x) for x in movements if 0<=sumTuple(pos,x)[0]<size and 0<=sumTuple(pos,x)[1]<size and sumTuple(pos,x) not in seen]
+    return [sumTuple(pos,x) for x in movements if legalPos(sumTuple(pos,x),seen)]
 
 def getReverse(word):
     words = []
     [words.extend([w[x:] for x in range(0,len(w))if len(w[x:])>2]) for w in [word,word[::-1]]]
     return words
 
-text=set()
-def rec(pos,seen):
+def exploreMatrix(pos,seen):
     neigh=getNeighbour(pos,seen)
-    if len(neigh)==0:
+    if not neigh:
         seen.append(pos)
-        [text.add(w) for w in getReverse(getWord(seen)) if w in dictionary]
+        [words.add(w) for w in getReverse(getWord(seen)) if w in dictionary]
     else:
         seen.append(pos)
-        for p in neigh:
-            rec(p,seen.copy())
-        
-for p in positions:
-    rec(p,[])
+        [exploreMatrix(p,seen.copy()) for p in neigh]
 
-print(sorted(text))
+
+matrixSize, words = 4, set()
+dictionary = [w.strip() for w in open("dictionary.txt")]
+positions = list(itertools.product(range(matrixSize),repeat=2))
+movements = [x for x in itertools.product(range(-1,2),repeat=2) if sum(x)==1 or sum(x)==-1]
+
+input = ["walk", "moon", "hate", "rope"] 
+[exploreMatrix(p,[]) for p in positions]
+
+print(sorted(words))
