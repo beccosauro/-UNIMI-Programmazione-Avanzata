fde66a9dc0bee54d7666e61a05c78d5f34564ca2
beccosauro <cerioli.simone@gmail.com>
2020-02-22 18:53:52 +0100

    shitty styff

--- ruzzle.py
+++ ruzzle.py
@@ -2,7 +2,7 @@ import itertools
 
 legalPos = lambda x, seen : 0<=x[0]<matrixSize and 0<=x[1]<matrixSize and x not in seen
 sumTuple = lambda x,y: tuple([sum(x) for x in zip(x,y)])
-getWord = lambda pos: "".join([input[x[0]][x[1]] for x in pos])
+getWord = lambda pos,input: "".join([input[x[0]][x[1]] for x in pos])
 
 def getNeighbour(pos,seen):
     return [sumTuple(pos,x) for x in movements if legalPos(sumTuple(pos,x),seen)]
@@ -16,7 +16,7 @@ def exploreMatrix(pos,seen):
     neigh=getNeighbour(pos,seen)
     seen.append(pos)
     if not neigh:
-        [words.add(w) for w in getReverse(getWord(seen)) if dictionary.get(w)]
+        [words.add(w) for w in getReverse(getWord(seen,input)) if dictionary.get(w)]
     else:
         [exploreMatrix(p,seen.copy()) for p in neigh]
 
@@ -26,6 +26,6 @@ dictionary = {w.strip():True for w in open("dictionary.txt")}
 positions = itertools.product(range(matrixSize),repeat=2)
 movements = [x for x in itertools.product(range(-1,2),repeat=2) if sum(x)==1 or sum(x)==-1]
 
-input = ["walk", "moon", "hate", "rope"] 
+input = ["poap", "lkjh", "aswe", "jhnh"]
 [exploreMatrix(p,[]) for p in positions]
 print(sorted(words))
